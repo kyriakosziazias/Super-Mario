076289121e64709f39fd9f159ab36bad6b9d1564
kyriakosziazias <32721043+kyriakosziazias@users.noreply.github.com>
2017-12-08 05:07:32 +0200

    Create MENU.js

---
+++ MENU.js
@@ -0,0 +1,28 @@
+var MMENU = {
+	preload: function() {
+
+		game.load.image('MENU', 'assets/MENU.png');
+		game.load.image('STAGE1', 'assets/STAGE1.png');
+		game.load.audio('intro', 'assets/The XX - Intro (Original Version).mp3');
+		},
+		
+		create: function() {
+		
+			intro = game.add.audio('intro');
+			intro.play();
+
+			game.scale.pageAlignHorizontally = true;
+			game.scale.pageAlignVertically = true;
+			game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+			game.physics.startSystem(Phaser.Physics.ARCADE);
+			
+			var MENUPic = game.add.sprite(8, 8, 'MENU');
+
+			var STAGEText = game.add.text(38, 150, 'STAGE1', {fontSize: '12px', fill: '#000'});
+			var click1 = game.add.button(60, 197, 'STAGE1', function() {
+			game.state.start('STAGE1');
+			intro.stop();
+		});
+		click1.anchor.set(0.5, 0.5);
+	}
+}
